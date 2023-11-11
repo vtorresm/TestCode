1d2b3d2b0ed5f068798ca3ba04ef4b6a2fe65720
Victor Torres <vatorresm88@gmail.com>
2023-11-11 00:27:35 -0500

    Actualizando usuario

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,10 +6,12 @@ import {
   Param,
   ParseIntPipe,
   Delete,
+  Patch,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -33,5 +35,14 @@ export class UsersController {
   @Delete(':id')
   deleteUser(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.deleteUser(id);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body()
+    user: UpdateUserDto,
+  ) {
+    return this.usersService.updateUser(id, user);
   }
 }
