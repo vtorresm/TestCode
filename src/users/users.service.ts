f065bcc7cd776b1df80a3dc53f88ec418cdbb8b6
Victor Torres <vatorresm88@gmail.com>
2023-11-13 16:14:34 -0500

    HTTP Exception UpdateUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -50,7 +50,16 @@ export class UsersService {
     return result;
   }
 
-  updateUser(id: number, user: UpdateUserDto) {
-    return this.userRepository.update({ id }, user);
+  async updateUser(id: number, user: UpdateUserDto) {
+    const userFound = await this.userRepository.findOne({
+      where: { id },
+    });
+
+    if (!userFound) {
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    const updateUser = Object.assign(userFound, user);
+    return this.userRepository.save(updateUser);
   }
 }
